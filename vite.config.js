df5e41a5bd43c9f315f94bda7f7a7e4a2a2670fb
Logan Stoltz <102991130+LoganStoltz@users.noreply.github.com>
2025-08-08 00:11:11 -0700

    Working on Deployment

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,12 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
-import svgLoader from 'vite-svg-loader'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-    svgLoader(),
-  ],
-  server: {
-    port: 3000,
-  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': path.resolve(__dirname, './src'),
     },
   },
+  plugins: [vue()],
 })
